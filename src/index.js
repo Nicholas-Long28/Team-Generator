c11976416d63e7ba2ff54824d84b699895805396
Nicholas Long <Nicholas.J.Long2018@gmail.com>
2021-05-09 18:09:41 -0700

    Updated manager,engineer,intern and rendering files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,119 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const Manager = require('./lib\manager.html');
+const Engineer = require('./lib\engineer.html');
+const Intern = require('./lib\intern.html');
+const { type } = require('node:os');
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-const promptUser = () => {
-    return inquirer.prompt([
+const promptManager = () => {
+    return inquirer.promt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Managers name:',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Manager ID',
+            validate: function(value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Managers Email',
+        },
+        {
+            type: 'input',
+            name: 'office number',
+            message: 'Manager Office Number',
+            validate: function(value) {
+                var valid = !isNaN(parseFloat(value));
+                return valid || 'Please enter a number';
+            },
+        },
         {
             type: 'list',
-            name: 'employee',
-            message: 'What type of worker do you want to add to the team?',
-            choices: ['Employee', 'Manager', 'Engineer', 'Intern']
+            name: 'another',
+            message: 'Would you like to add an Engineer, Intern, Employee or complete your team?',
+            choices: ['Engineer', 'Intern', 'Employee', 'None']
+        }
+    ])
+}
+
+const promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineers name?'
         },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Engineers ID?',
+            validate: function(value) {
+                var valid = !isNaN(parseFloat)(value);
+                return valid || 'Please enter a number';
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineers email?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Engineers Github',
+        },
+        {
+            type: 'list',
+            name: 'another',
+            message: 'Would you like to add another employee?',
+            choices: ['Engineer', 'intern', 'employee', 'none']
+        }
+    ])
+}
+
+const promptIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the employees name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employees ID number?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the employees email?',
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What role will the employee be performing?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school does the intern go to?',
+        }
+    ]);
+};
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -34,6 +136,73 @@ const promptUser = () => {
         },
     ]);
 };
+
+const team = [];
+
+function runEngineer(){
+    promptEngineer().then(function(response){
+        const engineer = new Engineer(rsponse.name, response.id, response.github, response.email)
+        if(response.another == 'Engineer'){
+            team.push(engineer);
+            runEngineer();
+        }else if(response.another == 'Intern'){
+            team.push(engineer);
+            runIntern();
+        }else if(response.another =='None'){
+            team.push(engineer);
+
+            fs.writeFile('./output/team.html', render(team),function(err){
+                if(err) throw err;
+                console.log('Writing team file..')
+            });
+            return;
+        }
+    })
+}
+
+function runIntern(){
+    promptIntern().then(function(response){
+        const engineer = new Intern(rsponse.name, response.id, response.github, response.email)
+        if(response.another == 'Engineer'){
+            team.push(Intern);
+            runEngineer();
+        }else if(response.another == 'Intern'){
+            team.push(Intern);
+            runIntern();
+        }else if(response.another =='None'){
+            team.push(Intern);
+
+            fs.writeFile('./output/team.html', render(team),function(err){
+                if(err) throw err;
+                console.log('Writing team file..')
+            });
+            return;
+        }
+    })
+}
+
+function runManager(){
+    promptManager().then(function(response){
+        const engineer = new Manager(rsponse.name, response.id, response.github, response.email)
+        if(response.another == 'Engineer'){
+            team.push(Manager);
+            runEngineer();
+        }else if(response.another == 'Intern'){
+            team.push(Manager);
+            runIntern();
+        }else if(response.another =='None'){
+            team.push(Manager);
+
+            fs.writeFile('./output/team.html', render(team),function(err){
+                if(err) throw err;
+                console.log('Writing team file..')
+            });
+            return;
+        }
+    })
+}
+
+runManager();
 
 const generateHTML = (answers) =>
     `<!DOCTYPE html>
